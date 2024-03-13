ec80c8bf68cec378f7d4073269b6ec4bc544c66a
Razioooooo <tanysang@126.com>
2024-03-13 11:45:56 +0800

    添加完整 Demo

--- SpatailPlane/SpatailPlane/GameViewController.swift
+++ SpatailPlane/SpatailPlane/GameViewController.swift
@@ -4,6 +4,7 @@ import SceneKit
 import SwiftUI
 import SwiftUIJoystick
 import Combine
+import PHASE
 
 public struct Joystick: View {
     
@@ -45,9 +46,18 @@ class GameViewController: UIViewController {
     @ObservedObject var monitor = JoystickMonitor()
     
     var cancel: Any?
+    
+    // PHASE
+    var engine: PHASEEngine!
+    var listener: PHASEListener!
+    var source: PHASESource!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 配置 PHASE 相关
+        configEngine()
         
         // create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
@@ -80,9 +90,6 @@ class GameViewController: UIViewController {
         
         // retrieve the ship node
         let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-//        // animate the 3d object
-//        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
         
         // retrieve the SCNView
         let scnView = self.view as! SCNView
@@ -119,6 +126,8 @@ class GameViewController: UIViewController {
             let x =  self.monitor.xyPoint.x / 10
             let z = self.monitor.xyPoint.y / 10
             
+            print("x: \(x), y: \(z)")
+            
             // 创建旋转矩阵
             
             // 计算从 p1 到 p2 的方向向量
@@ -134,12 +143,99 @@ class GameViewController: UIViewController {
             // 创建平移矩阵
             let translationMatrix = SCNMatrix4MakeTranslation(Float(x), 0, Float(z))
             ship.transform = SCNMatrix4Mult(rotationMatrix, translationMatrix)
+            
+            // 关联音源 与 飞机的位置
+            self.source.transform = simd_float4x4(translationMatrix)
 
         }
         
         // add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         scnView.addGestureRecognizer(tapGesture)
+    }
+    
+    func configEngine() {
+        
+        self.engine = PHASEEngine(updateMode: .automatic)
+        self.engine.defaultReverbPreset = .largeRoom
+        try? self.engine.start()
+
+        // Listener
+        self.listener = PHASEListener(engine: self.engine)
+        // 设置收听者的位置
+        self.listener.transform = matrix_identity_float4x4
+        // 添加到场景中
+        try? self.engine.rootObject.addChild(listener)
+        
+        // 设置播放源 Source
+        // 播放源形状
+        let mesh = MDLMesh.newIcosahedron(withRadius: 0.0142, inwardNormals: false, allocator:nil)
+        let shape = PHASEShape(engine: engine, mesh: mesh)
+        self.source = PHASESource(engine: engine, shapes: [shape])
+        // 设置播放源位置，
+        source.transform = matrix_identity_float4x4
+        // 添加到场景中
+        try? engine.rootObject.addChild(source)
+        
+        // 输出管道
+        let pipeline = PHASESpatialPipeline(flags: [.directPathTransmission, .lateReverb])!
+        pipeline.entries[PHASESpatialCategory.lateReverb]!.sendLevel = 0.1
+        
+        // 配置根据距离调节音量  
+        // PHASEGeometricSpreadingDistanceModelParameters 模拟随距离的声音损失的模型
+        let distanceModelParameters = PHASEGeometricSpreadingDistanceModelParameters()
+        // 我们在控制了飞机在 15 米为半径的圆内飞行
+        // 此处设置超过 16 米，超过 16 米声音渐隐
+        distanceModelParameters.fadeOutParameters = PHASEDistanceModelFadeOutParameters(cullDistance: 16)
+        distanceModelParameters.rolloffFactor = 0.3
+        
+        let spatialMixerDefinition = PHASESpatialMixerDefinition(spatialPipeline: pipeline)
+        spatialMixerDefinition.distanceModelParameters = distanceModelParameters
+        
+        
+        // 注册声音资源
+        let soundURL = Bundle.main.url(forResource: "plane", withExtension: "mp3")!
+        let soundAsset = try! self.engine.assetRegistry.registerSoundAsset(
+            url: soundURL,
+            identifier: "planeAsset",
+            assetType: .resident,
+            channelLayout: nil,
+            normalizationMode: .dynamic)
+        
+        // 创建采样器节点
+        let samplerNodeDefinition = PHASESamplerNodeDefinition(
+            soundAssetIdentifier: soundAsset.identifier,
+            mixerDefinition: spatialMixerDefinition
+        )
+        
+        samplerNodeDefinition.playbackMode = .looping
+        samplerNodeDefinition.setCalibrationMode(calibrationMode: .relativeSpl, level: 0)
+        samplerNodeDefinition.cullOption = .sleepWakeAtRealtimeOffset
+        
+        // 向引擎注册声音事件节点的资产来提供有关声音事件事件信息
+        let planeSoundEventAsset = try! engine.assetRegistry.registerSoundEventAsset(
+            rootNode: samplerNodeDefinition,
+            identifier: soundAsset.identifier + "_SoundEventAsset"
+        )
+        
+        // 通过为每个空间混音器配置 PHASEMixerParameters 对象来定义播放音频的声源以及收听音频的听者
+        let mixerParameters = PHASEMixerParameters()
+        mixerParameters.addSpatialMixerParameters(
+            identifier: spatialMixerDefinition.identifier,
+            source: source,
+            listener: listener
+        )
+        
+        // 要播放声音，请为每个节点生成一个 PHASESoundEvent 实例，并在声音事件上调用 start(completion:)
+        // 通过 PHASESoundEvent 初始化器 mixerParameters 参数传递空间混合器参数，将源与声音事件关联起来
+        let planeSoundEvent = try! PHASESoundEvent(
+            engine: engine,
+            assetIdentifier: planeSoundEventAsset.identifier,
+            mixerParameters: mixerParameters
+        )
+
+        planeSoundEvent.start()
+        
     }
     
     @objc
